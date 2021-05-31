9c93533c473ab1625262aacd45dc8670db7f09d6
Vitalii Cherenkov <WillardMoto@gmail.com>
2021-06-01 00:13:01 +0300

    fixed bugs, add slider at customers section

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
-
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    // If we need pagination
-    pagination: {
-    el: '.swiper-pagination',
-    },
-    // Navigation arrows
-    navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    },
+$(document).ready(function(){
+    $('.customers-slider').slick({
+        //centerMode: true,
+        //variableWidth: true,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        //fade: true,
+        arrows: false,
+        dots: false,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        });
 });
